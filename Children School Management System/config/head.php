5c923c7cf21087598f4442316fd29a50e6c122b2
Auwal Baba <96601071+Auwalbaba90@users.noreply.github.com>
2024-11-12 19:10:08 +0100

    Add files via upload

---
+++ Children School Management System/config/head.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+	<!-- ################# Header Starts Here #######################--->
+	<header id="menu-jk">
+	<div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#ff5050;font-weight:bold; font-size:25px; margin-top: 1% !important;"> <a href="index.php"><img style="size: 1px;" src="assets/images/gallery/naub-staff-children-school.png"></a>
+                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="./pages/login.php" target="block">Login</a></li>
+                            <li><a href="check_result.php">Check Result</a></li>
+                            <li><a href="index.php#about_us">About Us</a></li>
+                            <li><a href="index.php#gallery">Gallery</a></li>
+                            <li><a href="index.php#contact_us">Contact Us</a></li> 
+                        </ul>
+                    </div>
+                    <div class="col-sm-2 d-none d-lg-block appoint">
+                        <a class="btn btn-success" href="./pages/login.php" target="block">Staff Login</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+</body>
+</html>
