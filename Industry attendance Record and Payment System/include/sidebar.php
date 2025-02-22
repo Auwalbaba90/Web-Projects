0240e2c589f2bd6e5797141442e9e630cb011bd3
Auwal Baba <96601071+Auwalbaba90@users.noreply.github.com>
2025-02-22 16:58:15 +0100

    Add files via upload

---
+++ Industry attendance Record and Payment System/include/sidebar.php
@@ -0,0 +1,333 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-building-o"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Departments </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="departments.php">
+											<span class="title"> Add Department</span>
+										</a>
+									</li>
+									<li>
+										<a href="manage-departments.php">
+											<span class="title"> Manage Department </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-user-secret"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Staff </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="staff.php">
+											<span class="title"> Add Staff</span>
+										</a>
+									</li>
+									<li>
+										<a href="manage-staffs.php">
+											<span class="title"> Manage Staff </span>
+										</a>
+									</li>
+
+									<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-address-book"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Attendance </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="staff-attendance.php">
+											<span class="title"> Record Attendance</span>
+										</a>
+									</li>
+									<li>
+										<a href="staff-month-attendance.php">
+											<span class="title"> Manage Attendance </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-money"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Payroll </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="staff-payments-between-dates.php">
+											<span class="title"> Payment</span>
+										</a>
+									</li>
+									<li>
+										<a href="iou-payments.php">
+											<span class="title"> IOU </span>
+										</a>
+									</li>
+									<li>
+										<a href="staff-iou.php">
+											<span class="title"> IOU Invoice</span>
+										</a>
+									</li>
+									<li>
+										<a href="staff-loan.php">
+											<span class="title"> Staff Loan</span>
+										</a>
+									</li>
+									<li>
+										<a href="unpaid-staffs.php">
+											<span class="title"> Voucher </span>
+										</a>
+									</li>
+									<li>
+										<a href="paid-staffs.php">
+											<span class="title"> Paid Invoice </span>
+										</a>
+									</li>
+									<li>
+										<a href="staffpayslip.php">
+											<span class="title"> Pay Slip </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+									
+								</ul>
+								</li>
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-users"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Employees </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="employees.php">
+											<span class="title"> Add Employee</span>
+										</a>
+									</li>
+									<li>
+										<a href="manage-employees.php">
+											<span class="title"> Manage Employee </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-exchange"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Shifts </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="shifts.php">
+											<span class="title"> Add Shift</span>
+										</a>
+									</li>
+									<li>
+										<a href="manage-shifts.php">
+											<span class="title"> Manage Shift </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+							<li>
+								<a href="employee-card.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-id-card"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Employee Cards </span>
+										</div>
+									</div>
+								</a>
+							</li>	
+
+														<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-address-book"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Attendance </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="attendance.php">
+											<span class="title"> Record Attendance</span>
+										</a>
+									</li>
+									<li>
+										<a href="between-dates-attendance.php">
+											<span class="title"> Manage Attendance </span>
+										</a>
+									</li>
+									
+								</ul>
+							</li>
+
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-money"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Payroll </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="payments-between-dates.php">
+											<span class="title"> Payment </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="unpaid-between-dates.php">
+											<span class="title"> Unpaid Employee </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="paid-between-dates.php">
+											<span class="title"> Paid Employee </span>
+										</a>
+									</li>
+									
+								</ul>
+							</li>
+
+							<li>
+								<a href="admin-users.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-user-circle-o"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Admin Users </span>
+										</div>
+									</div>
+								</a>
+							</li>	
+	
+					
+						<!--	<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Reports </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="between-dates-reports.php">
+											<span class="title">B/w dates reports </span>
+										</a>
+									</li>
+								</ul>
+							</li>	-->
+
+
+							<li>
+								<a href="setting.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-cog"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Setting </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
